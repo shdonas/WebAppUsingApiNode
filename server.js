19ab8e89eb39667b031b8e15b9b38190d559109f
shdonas <BlackHole7473>
2020-05-18 18:55:50 -0400

    update

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 
+// const port = 3000;
+// const hostname = '192.168.1.183';
+
+// app.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
